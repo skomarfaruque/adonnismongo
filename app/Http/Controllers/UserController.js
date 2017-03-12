b0b7ce51bb96388d092f46dde2a54d70e1ec7f24
Sirajul <rianaville@gmail.com>
2017-03-12 18:21:56 +0600

    async function revert to generator

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -3,17 +3,17 @@ const User = use('App/Model/User')
 
 class UserController {
 
-  async index (req, res) {
-    let user = await User.create({
+  * index (req, res) {
+    let user = yield User.create({
       name: 'user',
       password: 'pass'
     })
-    return res.send(user)
+    res.send(user)
   }
 
-  async allUsers (req, res) {
-    let users = await User.find()
-    return res.send(users)
+  * allUsers (req, res) {
+    let users = yield User.find()
+    res.send(users)
   }
 }
 
