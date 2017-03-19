43754a30e84d2f84940b83fffd1ff8743e358936
Sirajul <rianaville@gmail.com>
2017-03-19 15:27:35 +0600

    Mongo provider settings file changed

--- providers/MongoProvider.js
+++ providers/MongoProvider.js
@@ -7,19 +7,19 @@ class MongoProvider extends ServiceProvider {
 
   * register () {
     this.app.singleton('Adonis/Addons/Mongoose', function (app) {
-        const Config = app.use('Adonis/Src/Config')
-        const mongoHost = Config.get('mongo.host', '127.0.0.1')
-        const mongoPort = Config.get('mongo.port', '27017')
-        const mongoDb = Config.get('mongo.db', 'test')
-        const mongoUser = Config.get('mongo.user', '')
-        const mongoPass = Config.get('mongo.pass', '')
+      const Env = use('Env')
+      const mongoHost = Env.get('MONGO_HOST', 'localhost')
+      const mongoPort = Env.get('MONGO_PORT', '27017')
+      const mongoDb = Env.get('MONGO_DATABASE', 'backportal-app')
+      const mongoUser = Env.get('MONGO_USER', '')
+      const mongoPass = Env.get('MONGO_PASS', '')
 
-        const connectUri = `${mongoHost}:${mongoPort}/${mongoDb}`
-        const connectionString = (mongoUser !== '' || mongoPass !== '') ? `${mongoUser}:${mongoPass}@${connectUri}` : connectUri
-        Mongoose.Promise = global.Promise
-        Mongoose.connect(connectionString)
+      const connectUri = `${mongoHost}:${mongoPort}/${mongoDb}`
+      const connectionString = (mongoUser !== '' || mongoPass !== '') ? `${mongoUser}:${mongoPass}@${connectUri}` : connectUri
+      Mongoose.Promise = global.Promise
+      Mongoose.connect(connectionString)
 
-        return Mongoose
+      return Mongoose
     })
   }
 
